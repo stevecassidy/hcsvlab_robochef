dab88093b4cdc474146f9efe5b4cbc92eac7acf4
Ilya Anisimoff <ilya@intersect.org.au>
2014-01-07 16:15:18 +1100

    MISC: Cleanses test.py

--- test.py
+++ test.py
@@ -11,57 +11,65 @@ from hcsvlab_robochef import annotations
 from hcsvlab_robochef import ace
 from hcsvlab_robochef import braided
 from hcsvlab_robochef import utils
-from hcsvlab_robochef import metadata 
-from hcsvlab_robochef import ice 
+from hcsvlab_robochef import metadata
+from hcsvlab_robochef import ice
 from hcsvlab_robochef import upload
 from hcsvlab_robochef import rdf
 
+
 class BasicTests(unittest.TestCase):
-  def testIsAlive(self):
-    """trivial test to get us going"""
-    self.assert_(True, "panic! how did this happen?")
+    def testIsAlive(self):
+        """trivial test to get us going"""
+        self.assert_(True, "panic! how did this happen?")
+
 
 class AdhocTests(unittest.TestCase):
-  def testExpectedFiles(self):
-    """Test all the example output files we have in the adhoctests directory against the output generated from the last run of the ingest program"""
-    for c in utils.getCorporaArgs(sys.argv):
-      res = []
-      utils.listFiles(res, "../systemtests/" + c)
-      for f in res:
-        if (f.find(".DS_Store") < 0):  # todo: find a cleaner way to exclude files like the DS_Store (silly macos)
-          result = subprocess.call(["diff", "-w", f, "../output/" + f[14:]])  #need to remove first 14 letters since ../adhoctests is tacked on the front
-          self.assert_((result == 0), ("diff failed for " + f + "\n to see output run diff -w "+f+" ../output/" + f[14:]))
+    def testExpectedFiles(self):
+        """
+        Test all the example output files we have in the adhoctests directory against the output generated from the last
+         run of the ingest program.
+        """
+        for c in utils.getCorporaArgs(sys.argv):
+            res = []
+            # utils.listFiles(res, "../systemtests/" + c)
+            utils.listFiles(res, '.')
+            for f in res:
+                if (f.find(
+                        ".DS_Store") < 0):  # todo: find a cleaner way to exclude files like the DS_Store (silly macos)
+                    result = subprocess.call(["diff", "-w", f, "../output/" + f[
+                                                                              14:]])  #need to remove first 14 letters since ../adhoctests is tacked on the front
+                    self.assert_((result == 0), (
+                    "diff failed for " + f + "\n to see output run diff -w " + f + " ../output/" + f[14:]))
 
-if __name__ == "__main__":
-	main()
-	adhoc()
-	
 def main():
-  suite = unittest.TestSuite()
-  suite.addTests([BasicTests("testIsAlive")        \
-                , monash.unittests()               \
-                , cooee.unittests()                \
-                , griffith.unittests()             \
-                , ice.unittests()                  \
-                , utils.unittests()                \
-                , annotations.unittests()          \
-                , ace.unittests()                  \
-                , braided.unittests()              \
-                , utils.unittests()                \
-                , metadata.unittests()             \
-                , upload.unittests()               \
-                , rdf.unittests()                  \
-                ])
-  unittest.TextTestRunner(verbosity=2).run(suite)
+    suite = unittest.TestSuite()
+    suite.addTests([BasicTests("testIsAlive"),
+                    monash.unittests(),
+                    cooee.unittests(),
+                    # griffith.unittests(),
+                    ice.unittests(),
+                    utils.unittests(),
+                    annotations.unittests(),
+                    ace.unittests(),
+                    braided.unittests(),
+                    utils.unittests(),
+                    metadata.unittests(),
+                    upload.unittests(),
+                    rdf.unittests()])
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
 
 def current():
-  suite = unittest.TestSuite()
-  suite.addTests([BasicTests("testIsAlive")        \
-                , monash.unittests()               \
-                ])
-  unittest.TextTestRunner(verbosity=2).run(suite)
-    
+    suite = unittest.TestSuite()
+    suite.addTests([BasicTests("testIsAlive"), monash.unittests()])
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
 def adhoc():
-  suite = unittest.TestSuite()
-  suite.addTests([AdhocTests("testExpectedFiles")])
-  unittest.TextTestRunner(verbosity=2).run(suite)
+    suite = unittest.TestSuite()
+    suite.addTests([AdhocTests("testExpectedFiles")])
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+if __name__ == "__main__":
+    main()
+    # adhoc()

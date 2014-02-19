2c1ab6043af8817ad493fe4e14d1b4e47f8c9e72
matthew-intersect <matthew@intersect.org.au>
2014-02-19 11:35:32 +1100

    HCSVLAB-810 Add manifest file creation

--- main.py
+++ main.py
@@ -41,7 +41,8 @@ def main():
             print "converting ART"
             art = ARTIngest()
             art.setMetaData(corpus_basedir + "ART/ART-corpus-catalogue.xls")
-            art.ingestCorpus(corpus_basedir + "ART", output_dir + "ART")
+            art.ingestCorpus(corpus_basedir + "ART", output_dir + "art")
+            art.create_collection_manifest(output_dir + "art", "turtle")
 
         elif c == "cooee":
             print "converting COOEE"
@@ -49,6 +50,7 @@ def main():
             cooee.setMetaData(corpus_basedir + "COOEE/COOEE.XLS")
             cooee.copy_collection_metadata(corpus_basedir, output_dir, "cooee.n3", "cooee.n3")
             cooee.ingestCorpus(corpus_basedir + "COOEE/data", output_dir + "cooee")
+            cooee.create_collection_manifest(output_dir + "cooee", "turtle")
 
         elif c == "ace":
             print "converting ace"
@@ -56,12 +58,14 @@ def main():
             ace.setMetaData(corpus_basedir + "ace/Manual")
             ace.copy_collection_metadata(corpus_basedir, output_dir, "ace.n3", "ace.n3")
             ace.ingestCorpus(corpus_basedir + "ace", output_dir + "ace")
+            ace.create_collection_manifest(output_dir + "ace", "turtle")
 
         elif c == "ice":
             print "converting ice"
             ice = ICEIngest()
             ice.copy_collection_metadata(corpus_basedir, output_dir, "ice.n3", "ice.n3")
             ice.ingest(os.path.join(corpus_basedir, "ICE"), os.path.join(output_dir, "ice"))
+            ice.create_collection_manifest(output_dir + "ice", "turtle")
 
         elif c == "monash":
             print "converting monash"
@@ -69,6 +73,7 @@ def main():
             monash.copy_collection_metadata(corpus_basedir, output_dir, "monash.n3", "monash.n3")
             monash.ingestCorpus(corpus_basedir + "Monash Corpus of Australian English/Transcripts-sanitised",
                                 output_dir + "monash")
+            monash.create_collection_manifest(output_dir + "monash", "turtle")
 
         elif c == "griffith":
             print "converting griffith"
@@ -76,6 +81,7 @@ def main():
             griffith.setMetaData(corpus_basedir + "griffith/metadata")
             griffith.copy_collection_metadata(corpus_basedir, output_dir, "gcsause.n3", "gcsause.n3")
             griffith.ingestCorpus(corpus_basedir + "griffith", output_dir + "griffith")
+            griffith.create_collection_manifest(output_dir + "griffith", "turtle")
 
         elif c == "md":
             print "converting mitchell & delbridge"
@@ -83,53 +89,65 @@ def main():
             md.setMetaData(corpus_basedir + "MD/flatfilesrc.txt")
             md.copy_collection_metadata(corpus_basedir, output_dir, "md.n3", "mitcheldelbridge.n3")
             md.ingestCorpus(corpus_basedir + "MD", output_dir + "md")
+            md.create_collection_manifest(output_dir + "md", "turtle")
 
         elif c == "auslit":
             print "converting auslit"
             auslit = AuslitIngest()
             auslit.copy_collection_metadata(corpus_basedir, output_dir, "austlit.n3", "austlit.n3")
             auslit.ingestCorpus(corpus_basedir + "AusLit", output_dir + "auslit")
+            auslit.create_collection_manifest(output_dir + "auslit", "turtle")
 
         elif c == "braided":
             print "converting braided channels"
             braided = BraidedIngest()
             braided.copy_collection_metadata(corpus_basedir, output_dir, "braided_channels.n3", "braidedchannels.n3")
             braided.ingestCorpus(corpus_basedir + "braided_channels", output_dir + "braided")
+            braided.create_collection_manifest(output_dir + "braided", "turtle")
 
         elif c == "paradisec":
             print "converting paradisec"
             paradisec = ParadisecIngest()
             paradisec.ingestCorpus(corpus_basedir + "paradisec", output_dir + "paradisec")
+            paradisec.create_collection_manifest(output_dir + "paradisec", "turtle")
 
         elif c == "eopas_test":
             print "converting eopas_test"
             eopas = EopasTestIngest()
             eopas.ingestCorpus(corpus_basedir + "eopas_test", output_dir + "eopas_test")
+            eopas.create_collection_manifest(output_dir + "eopas_test", "turtle")
 
         elif c == "avozes":
             print "converting avozes"
             avozes = AvozesIngest()
             avozes.setMetaData(corpus_basedir + "avozes/Avozes.xls")
             avozes.ingestCorpus(corpus_basedir + "avozes", output_dir + "avozes")
+            avozes.create_collection_manifest(output_dir + "avozes", "turtle")
 
         elif c == "pixar":
             print "converting Pixar Film Excerpts"
             pixar = PixarIngest()
-            pixar.setMetaData(corpus_basedir+"pixar/PixarFilmExcerpts.xls")
+            pixar.setMetaData(corpus_basedir + "pixar/PixarFilmExcerpts.xls")
             pixar.copy_collection_metadata(corpus_basedir, output_dir, "pixar.n3", "pixar.n3")
-            pixar.ingestCorpus(corpus_basedir+"pixar", output_dir+"pixar")
+            pixar.ingestCorpus(corpus_basedir + "pixar", output_dir + "pixar")
+            pixar.create_collection_manifest(output_dir + "pixar", "turtle")
 
         elif c == "austalk":
             print "converting austalk"
             austalk = AustalkIngest()
             austalk.ingestCorpus(corpus_basedir + "austalk", output_dir + "austalk")
+            for item in os.listdir(output_dir + "austalk"):
+                subdir = os.path.join(output_dir + "austalk", item)
+                if os.path.isdir(subdir):
+                    austalk.create_collection_manifest(subdir, "nt") 
 
         elif c == "rirusyd":
             print "converting Room Impulse Response"
             rir = RirIngest()
-            rir.setMetaData(corpus_basedir+"rirusyd/room_impulse_responses.xlsx")
+            rir.setMetaData(corpus_basedir + "rirusyd/room_impulse_responses.xlsx")
             rir.copy_collection_metadata(corpus_basedir, output_dir, "rirusyd.n3", "rirusyd.n3")
-            rir.ingestCorpus(corpus_basedir+"rirusyd", output_dir+"rirusyd")
+            rir.ingestCorpus(corpus_basedir + "rirusyd", output_dir + "rirusyd")
+            rir.create_collection_manifest(output_dir + "rirusyd", "turtle")
 
 if __name__ == "__main__":
     main()
